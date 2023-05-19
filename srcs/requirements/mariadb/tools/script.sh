5d054f2390dad190c935740307ce58c22be36085
taoufik bouzalmat <tbouzalm@e2r1p1.1337.ma>
2023-05-19 23:17:32 +0100

    still working in script mariadb

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
-service mysql start 
 
 echo "il est entreeeer"
+
+service mysql start
+
+echo "start mysql"
+#mysql command [mysql -u root -p] pour l'instant j'ai pas besoin de cette command (root ou password)
+
+# mysql -e "CREATE DATABASE $MYSQL_DATABASE;"
+mysql -e "CREATE DATABASE "wp";"
+# mysql -e "create users '$MYSQL_USER'@'localhost' identifed by '$MYSQL_PASSWORD';"
+mysql -e "CREATE USER 'toufiq'@'localhost' IDENTIFIED BY 'PASSWORD';"
+
+#GRANT ALL PRIVILEGES ON test TO 'sammy'@'localhost' WITH GRANT OPTION;
+mysql -e "GRANT ALL PRIVILEGES ON wp.* TO 'toufiq'@'localhost' WITH GRANT OPTION;"
+
+mysql -e "flush privileges;"
+
+# service mysql stop
+# kill $(cat /var/run/mysqld/mysqld.pid)
+
+# sleep 100
+mysql -e"show databases;"
+
+
+kill $(cat /var/run/mysqld/mysqld.pid) 
+mysqld
+
