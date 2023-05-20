372997a73c225f0c444691059f7911c13404a9cb
taoufik bouzalmat <tbouzalm@e2r1p5.1337.ma>
2023-05-20 13:33:53 +0100

    update in mariadb path and (il faut supprimer le cach tjr aveck la command docker-copose build --no-cache && docker-compose up)

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -8,23 +8,31 @@ service mysql start
 echo "start mysql"
 #mysql command [mysql -u root -p] pour l'instant j'ai pas besoin de cette command (root ou password)
 
-# mysql -e "CREATE DATABASE $MYSQL_DATABASE;"
-mysql -e "CREATE DATABASE "wp";"
-# mysql -e "create users '$MYSQL_USER'@'localhost' identifed by '$MYSQL_PASSWORD';"
-mysql -e "CREATE USER 'toufiq'@'localhost' IDENTIFIED BY 'PASSWORD';"
+# source ""
 
-#GRANT ALL PRIVILEGES ON test TO 'sammy'@'localhost' WITH GRANT OPTION;
-mysql -e "GRANT ALL PRIVILEGES ON wp.* TO 'toufiq'@'localhost' WITH GRANT OPTION;"
+# export MYSQL_ROOT_PASSWORD=1234
+# export MYSQL_DATABASE=wp_db
+# export MYSQL_USER=wp_user
+# export MYSQL_PASSWORD=1234
+
+echo "$MYSQL_DATABASE"
+
+mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+
+mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+
+# mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABSE.* TO '$MYSQL_USER'@'localhost' WITH GRANT OPTION;"
+mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" 
 
 mysql -e "flush privileges;"
 
-# service mysql stop
 # kill $(cat /var/run/mysqld/mysqld.pid)
 
-# sleep 100
-mysql -e"show databases;"
+mysql -e "show databases;"
+# service mysql stop
 
+# sleep 10000
 
-kill $(cat /var/run/mysqld/mysqld.pid) 
+# kill $(cat /var/run/mysqld/mysqld.pid) 
 mysqld
 
