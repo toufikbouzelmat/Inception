24be5be16bfafeb85243422d0a79e84162e1cf1e
taoufik bouzalmat <tbouzalm@e2r1p1.1337.ma>
2023-05-27 05:20:04 +0100

    finish nginx with certificate ssl && now working to display page wordpress in nav

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# yes "" | openssl req -newkey rsa:2048 -nodes -keyout /etc/nginx/tbouzalm.42.fr.key -x509 -days 365 -out /etc/nginx/tbouzalm.42.fr.crt
 
-if [ ! -f /etc/ssl/certs/nginx.crt ]; then
-echo "Nginx: setting up ssl ...";
-openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
-		-keyout /etc/ssl/private/nginx.key \
-		-out /etc/ssl/certs/nginx.crt \
-		-subj "/C=MA/CN=tbouzalm.42.fr";
-echo "Nginx: ssl is set up!";
-fi
+openssl req -x509 -newkey rsa:2048 -days 365 -nodes -keyout  /etc/ssl/certs/ssl_key.key -out /etc/ssl/certs/ssl_prv.crt -subj "/C=MA/CN=tbouzalm.42.fr"
