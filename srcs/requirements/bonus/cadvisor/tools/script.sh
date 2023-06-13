ce13d98fd044f87a093801918cf04e0707138d38
taoufik bouzalmat <tbouzalm@e2r1p5.1337.ma>
2023-06-13 02:23:56 +0100

    all services work with succuss

--- srcs/requirements/bonus/cadvisor/tools/script.sh
+++ srcs/requirements/bonus/cadvisor/tools/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -fsSl "https://github.com/google/cadvisor/releases/download/v0.39.1/cadvisor" -o /usr/bin/cadvisor --silent
+curl -fsSL "https://github.com/google/cadvisor/releases/download/v0.39.1/cadvisor" -o /usr/bin/cadvisor --silent
 
 chmod +x /usr/bin/cadvisor
