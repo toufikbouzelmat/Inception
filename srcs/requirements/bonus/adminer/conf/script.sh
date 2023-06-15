98f06909e5504b82af82b1a0fbe73ffbc5bc14a6
taoufik bouzalmat <tbouzalm@e2r1p1.1337.ma>
2023-06-15 01:13:00 +0100

    projet inception valide with success 105

--- srcs/requirements/bonus/adminer/conf/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir "/adminer"
-
-wget "https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-en.php" -O /adminer/index.php
--- srcs/requirements/bonus/cadvisor/tools/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -fsSL "https://github.com/google/cadvisor/releases/download/v0.39.1/cadvisor" -o /usr/bin/cadvisor --silent
-
-chmod +x /usr/bin/cadvisor
